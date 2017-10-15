19fb8c7d194eab26bfc3d53da5a2b97186dfbd37
Egor Dmitriev <egordmitriev2@gmail.com>
2017-10-15 07:33:53 +0200

    Implemented page scrapping

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
+
 sys.path.insert(0, 'ml')
 
 import logic.ml as mlogic
+import logic.crawler as crawler
 from flask import Flask, jsonify
-from flask import request,render_template
+from flask import request, render_template
 from logic.url_tools import valid_url
-
 
 app = Flask(__name__)
 
-model_path = 'saves/my_model/save.model'
+model = 'hybrid'
+model_path = 'saves/{}/save.model'.format(model)
 
 
 @app.route('/')
@@ -21,14 +23,21 @@ def index():
 def classify():
     url = request.form['url']
     if not valid_url(url): return make_error('Not valid url!')
-    # TODO: scrap from this url
-    data = 'We are sittig here at the hackathon doing koekeloeren and we still have 12 hours to go.'
+    scrap_data = crawler.scrap_page(url)
+    data = crawler.special_to_text(scrap_data)
     positive = mlogic.classify(data, model_path)
     return jsonify({
         'success': True,
-        'positive': True
+        'positive': bool(positive),
+        'preview': crawler.special_to_html(scrap_data)
     })
 
+
+@app.route('/preview', methods=['POST'])
+def preview():
+    url = request.form['url']
+    if not valid_url(url): return make_error('Not valid url!')
+    return str(crawler.scrap_page(url))
 
 
 def make_error(message):
